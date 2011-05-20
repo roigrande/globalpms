d1e33c925a80971f579f9c25d009d72e421258ac
chenko <chenko@chenko-Vostro-3300>
2011-05-20 10:23:20 +0200

    ADD Logs album project

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,10 +2,22 @@
 
 class IndexController extends Zend_Controller_Action
 {
-    
+//    function __construct()
+//	{
+//		
+//        parent::_construct();
+//         //Inicializamos el log
+//        $logger = new Zend_Log();
+//        //Aqui ponemos las salida por archivo
+//        $writer = new Zend_Log_Writer_Stream('../log/zfw.log');
+//        $logger->addWriter($writer);
+//        //Aqui indicamos que solo se mostraran los mensajes que sean iguales o superiores a criticos
+//        $filter = new Zend_Log_Filter_Priority(Zend_Log::CRIT);
+//        $this->logger=$logger->addFilter($filter);
+//	}
     public function init()
     {
-        /* Initialize action controller here */
+        
     }
 
     function indexAction()
@@ -27,8 +39,24 @@ class IndexController extends Zend_Controller_Action
                 $artist = $form->getValue('artist');
                 $title = $form->getValue('title');
                 $albums = new Application_Model_DbTable_Albums();
-                $albums->addAlbum($artist, $title);
+                $albums->addAlbum($artist, $title);      
+                               
+                //Inicializamos el log
+                $logger = new Zend_Log();
+                //Aqui ponemos las salida por archivo TODO pasar al application.ini
+                $writer = new Zend_Log_Writer_Stream('../log/zfw.log');
+                $logger->addWriter($writer);
+                //Aqui indicamos que solo se mostraran los mensajes que sean iguales o superiores a criticos
+                $filter = new Zend_Log_Filter_Priority(Zend_Log::INFO);
+                $logger=$logger->addFilter($filter);
+                
+                //mensajes para log   
+                $logger->emerg('alta cancion del artista '.$artist);
+                $logger->debug('alta cancion con titulo '.$title);
+                
                 $this->_helper->redirector('index');
+                //Escribimos un simple mensaje 
+
             } else {
                 $form->populate($formData);
             }
@@ -49,7 +77,18 @@ class IndexController extends Zend_Controller_Action
                 $title = $form->getValue('title');
                 $albums = new Application_Model_DbTable_Albums();
                 $albums->updateAlbum($id, $artist, $title);
-
+                
+                //Inicializamos el log
+                $logger = new Zend_Log();
+                //Aqui ponemos las salida por archivo
+                $writer = new Zend_Log_Writer_Stream('../log/zfw.log');
+                $logger->addWriter($writer);
+                //Aqui indicamos que solo se mostraran los mensajes que sean iguales o superiores al filtro
+                $filter = new Zend_Log_Filter_Priority(Zend_Log::INFO);
+                $logger->addFilter($filter);
+                
+                $logger->alert('actualiza la informacion del title '.$title);
+                              
 
                 $this->_helper->redirector('index');
             } else {
@@ -73,6 +112,19 @@ public function deleteAction()
             $id = $this->getRequest()->getPost('id');
             $albums = new Application_Model_DbTable_Albums();
             $albums->deleteAlbum($id);
+            
+            //Inicializamos el log
+            $logger = new Zend_Log();
+            //Aqui ponemos las salida por archivo
+            $writer = new Zend_Log_Writer_Stream('../log/zfw.log');
+            $logger->addWriter($writer);
+            //Aqui indicamos que solo se mostraran los mensajes que sean iguales o superiores a criticos
+            $filter = new Zend_Log_Filter_Priority(Zend_Log::INFO);
+            $this->logger=$logger->addFilter($filter);
+            
+          
+            $logger->notice('elimnar el album con id '.$id);
+           
         }
         $this->_helper->redirector('index');
     } else {

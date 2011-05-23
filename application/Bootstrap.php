242d0142b16858da3aed21840a543fb038554d6f
agustincl <agustincl@gmail.com>
2011-05-23 19:29:24 +0200

    General Bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,16 +1,16 @@
 <?php
+/**
+ * Application bootstrap
+ *
+ * @uses    Zend_Application_Bootstrap_Bootstrap
+ * @package Iptours
+ * @version SVN $Id: Bootstrap.php 1457 2010-07-06 10:33:27Z agustincl $
+ */
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
-
-
- 	protected function _initAutoload()
-    {
-        $autoloader = new Zend_Application_Module_Autoloader(array(
-            'namespace' => 'globalpms',
-            'basePath'  => dirname(__FILE__),
-        ));
-        return $autoloader;
-    }
+ {
+ 	
+ 	
+    
+	
 }
-

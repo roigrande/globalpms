67cfef641550d925b034f845e06330e56c28e6cc
agustincl <agustincl@gmail.com>
2011-05-23 18:13:03 +0200

    Default controlles

---
+++ application/modules/default/controllers/IndexController.php
@@ -0,0 +1,37 @@
+<?php
+
+class Default_IndexController extends Zend_Controller_Action
+{
+//    function __construct()
+//	{
+//		
+//        parent::_construct();
+//         //Inicializamos el log
+//        $logger = new Zend_Log();
+//        //Aqui ponemos las salida por archivo
+//        $writer = new Zend_Log_Writer_Stream('../log/zfw.log');
+//        $logger->addWriter($writer);
+//        //Aqui indicamos que solo se mostraran los mensajes que sean iguales o superiores a criticos
+//        $filter = new Zend_Log_Filter_Priority(Zend_Log::CRIT);
+//        $this->logger=$logger->addFilter($filter);
+//	}
+    public function init()
+    {
+        
+    }
+
+    function indexAction()
+    {
+
+    }
+
+   
+
+}
+
+
+
+
+
+
+

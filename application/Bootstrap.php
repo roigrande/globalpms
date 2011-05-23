56b33a3c5cc569a31c82ac3eed3bcb1adebb22a0
agustincl <agustincl@gmail.com>
2011-05-23 18:13:27 +0200

    Application

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -4,5 +4,13 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
 
+ 	protected function _initAutoload()
+    {
+        $autoloader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => 'globalpms',
+            'basePath'  => dirname(__FILE__),
+        ));
+        return $autoloader;
+    }
 }
 
